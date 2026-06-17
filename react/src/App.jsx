13fe688277bca8b774a6a8978f77d836ff74503f
Tachi <shun>
2026-06-17 12:13:33 +0900

    useStateで管理

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,24 +1,37 @@
+import { useState } from "react";
 import TodoItem from "./components/TodoItem";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
   return (
     <div className="container">
       <h1>React TODOアプリ</h1>
 
-      <form>
-        <input type="text" placeholder="何をする？" />
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input 
+          type="text" 
+          placeholder="何をする？" 
+          value={inputText}
+          onChange={handleInputChange}
+        />
         <button type="submit">追加</button>
       </form>
+
+      <p style={{ color: "#666" }}>入力中のテキスト: {inputText}</p>
 
       <hr />
 
       <ul className="todo-list">
         <TodoItem task="Reactのコンポーネントを学ぶ" />
         <TodoItem task="WordPressの資料を作る" />
-        <TodoItem task="お気に入りのIPAビールを買う" />
       </ul>
     </div>
   );
-}
+};
 
 export default App;
